1ee96f97c849787e0edf0a475a838c03c0bb5614
RenadAlqarni <renadsalqarni@gmail.com>
2023-12-20 15:13:01 +0300

    Initial Commit

---
+++ JournalingApp/JournalingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalingAppApp.swift
+//  JournalingApp
+//
+//  Created by Renad Alqarni on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct JournalingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
